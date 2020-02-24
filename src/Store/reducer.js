e642690a825402ce081a202c72d8a0096d5b672e
keith <xqk559@gmail.com>
2020-02-24 08:55:44 -0500

    Dispatch actions via button in counter.js

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -1,8 +1,13 @@
 const initialState= {
-    counter : 0,
+    counter: 0,
 }
 
 const reducer = (state = initialState, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        }
+    }
     return state;
 };
 
