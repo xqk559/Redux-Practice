b1e569657461e9965374bf6d14599bc037e0cd0b
keith <xqk559@gmail.com>
2020-02-24 09:13:49 -0500

    Replaced if else in reducer with switch statement

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -3,22 +3,23 @@ const initialState= {
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        };
-    } else if (action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1
-        };
-    } else if (action.type === 'ADDFIVE') {
-        return {
-            counter: state.counter + action.val
-        };
-    } else if (action.type === 'LESSFIVE') {
-        return {
-            counter: state.counter - action.val
-        };
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            }
+        case 'ADDFIVE':
+            return {
+                counter: state.counter + action.val
+            }
+        case 'LESSFIVE':
+            return {
+                counter: state.counter - action.val
+            }
     }
     
     return state;
