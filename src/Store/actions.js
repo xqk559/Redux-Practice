adddc8ade8f56d0e9d62e11bbdd78dae0159be9a
keith <xqk559@gmail.com>
2020-02-25 08:09:02 -0500

    Made actions.js so actions in reducer are variables instead of strings and will throw errors on typos

---
+++ src/Store/actions.js
@@ -0,0 +1,8 @@
+export const INCREMENT= 'INCREMENT';
+export const DECREMENT= 'DECREMENT';
+export const ADD = 'ADD';
+export const SUBTRACT = 'SUBTRACT';
+export const ADDFIVE = 'ADDFIVE';
+export const LESSFIVE = 'LESSFIVE';
+export const STORE_RESULT= 'STORE_RESULT';
+export const DELETE_RESULT= 'DELETE_RESULT';
--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from './actions';
+
 const initialState= {
     counter: 0,
     results: [],
@@ -5,31 +7,31 @@ const initialState= {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'INCREMENT':
+        case actionTypes.INCREMENT:
             const newState = Object.assign({}, state);
             newState.counter = state.counter + 1;
             return newState;
-        case 'DECREMENT':
+        case actionTypes.DECREMENT:
             return {
                 ...state,
                 counter: state.counter - 1
             }
-        case 'ADDFIVE':
+        case actionTypes.ADDFIVE:
             return {
                 ...state,
                 counter: state.counter + action.val
             }
-        case 'LESSFIVE':
+        case actionTypes.LESSFIVE:
             return {
                 ...state,
                 counter: state.counter - action.val
             }
-        case 'STORE_RESULT':
+        case actionTypes.STORE_RESULT:
             return {
                 ...state,
                 results: state.results.concat({id: new Date(), value: state.counter})
             }
-        case 'DELETE_RESULT':
+        case actionTypes.DELETE_RESULT:
             // const id = 2;
             // const newArray = [...state.results];
             // newArray.splice(id, 1)
