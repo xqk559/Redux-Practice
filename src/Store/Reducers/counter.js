cea408746d0796323742acdd251d1242ad1c1510
keith <xqk559@gmail.com>
2020-02-26 10:31:37 -0500

    Added utility function to shorten code in action creators

--- src/Store/Reducers/counter.js
+++ src/Store/Reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../../Store/actions/actions';
+import {updateObject} from '../utility';
 
 const initialState= {
     counter: 0,
@@ -11,20 +12,11 @@ const reducer = (state = initialState, action) => {
             newState.counter = state.counter + 1;
             return newState;
         case actionTypes.DECREMENT:
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
+            return updateObject(state, {counter: state.counter - 1});
         case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.val
-            }
+            return updateObject(state, {counter: state.counter + action.val});
         case actionTypes.SUBTRACT:
-            return {
-                ...state,
-                counter: state.counter - action.val
-            }
+            return updateObject(state, {counter: state.counter - action.val});
     }
     return state;
 };
--- src/Store/Reducers/result.js
+++ src/Store/Reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../../Store/actions/actions';
+import {updateObject} from '../utility';
 
 const initialState= {
     results: [],
@@ -7,19 +8,13 @@ const initialState= {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: action.result})
-            }
+            return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
         case actionTypes.DELETE_RESULT:
             // const id = 2;
             // const newArray = [...state.results];
             // newArray.splice(id, 1)
             const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return updateObject(state, {results: updatedArray});
     }
     return state;
 };
