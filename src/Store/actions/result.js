a239a5a95e1ce5007cbeb587a7029e7ff700dd64
keith <xqk559@gmail.com>
2020-02-26 09:58:18 -0500

    Put action creators into seperate files and made an index.js to export them all

---
+++ src/Store/actions/result.js
@@ -0,0 +1,23 @@
+import * as actionTypes from './actions';
+
+export const saveResult = (result) => {
+    return {
+        type: actionTypes.STORE_RESULT,
+        result: result,
+    };
+}
+
+export const storeResult = (result) => {
+    return dispatch => {
+        setTimeout(()=>{
+            dispatch(saveResult(result));
+        }, 2000);
+    }
+};
+
+export const deleteResult = (resElId) => {
+    return {
+        type: actionTypes.DELETE_RESULT,
+        resultElId: resElId,
+    };
+};
