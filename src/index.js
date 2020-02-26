91da7818552a3ff210f264967f52aaab0bb1dc42
keith <xqk559@gmail.com>
2020-02-26 07:42:23 -0500

    Added Middleware to index.js to log actions on the state

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import counterReducer from './Store/Reducers/counter';
 import resultsReducer from './Store/Reducers/result';
@@ -13,7 +13,18 @@ const rootReducer = combineReducers({
     res: resultsReducer,
 });
 
-const store = createStore(rootReducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('Middleware Dispatching', action);
+            const result = next(action);
+            console.log('Middleware next state', store.getState());
+            return result;
+        }
+    }
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
